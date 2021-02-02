363d9d2ae4cc9159f6791bb4366a776a31483997
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-02-02 12:36:46 +0100

    php-ajax-dischi inizio

---
+++ milestone1/index.php
@@ -0,0 +1,17 @@
+<!-- Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: 
+al caricamento della pagina ci saranno tutti i dischi. -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ milestone2/index.php
@@ -0,0 +1,19 @@
+<!-- Seconda Milestone:
+Attraverso l’utilizzo di AJAX: al caricamento della pagina ajax chiederà attraverso 
+una chiamata i dischi a php e li stamperà attraverso VueJS. 
+-->
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
