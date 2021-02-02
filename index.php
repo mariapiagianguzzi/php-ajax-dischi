c8fb3515cdc4aa6b9bbefffd6da46842f6bebfa5
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-02-02 17:09:13 +0100

    php-ajax-dischi logo

--- index.php
+++ index.php
@@ -78,6 +78,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dist/css/style.css">
     <title>Ajax Dischi</title>
 </head>
 <body>
